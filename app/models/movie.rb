4d9fe89fefa986203d0dee4e5a9198420c0d6969
Ramone Robertson <ramonerobertson10@gmail.com>
2024-09-21 20:30:22 +0900

    corrected validation for runtime in movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,7 +2,7 @@ class Movie < ApplicationRecord
   # Validations
   validates :title, :overview, :poster_url, :release_date, presence: true
   validates :rating, :runtime, numericality: true
-  validates :runtime, numericality: { onliy_integer: true }
+  validates :runtime, numericality: { only_integer: true }
 
   # Associations
   has_many :bookmarks
