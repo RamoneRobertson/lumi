35071ec31179edd101d1fbec2fb8c50563e3ee6a
Ramone Robertson <ramonerobertson10@gmail.com>
2024-09-21 17:02:58 +0900

    Added list_id foreign key to bookmarks

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_21_075100) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_21_075513) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.bigint "movie_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "list_id"
+    t.index ["list_id"], name: "index_bookmarks_on_list_id"
+    t.index ["movie_id"], name: "index_bookmarks_on_movie_id"
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.bigint "bookmark_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["bookmark_id"], name: "index_lists_on_bookmark_id"
+    t.index ["user_id"], name: "index_lists_on_user_id"
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -50,6 +69,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_21_075100) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookmarks", "lists"
+  add_foreign_key "bookmarks", "movies"
+  add_foreign_key "lists", "bookmarks"
+  add_foreign_key "lists", "users"
   add_foreign_key "reviews", "movies"
   add_foreign_key "reviews", "users"
 end
