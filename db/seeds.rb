9b2eb2be43d38ff0423f6d030fff3b19ff4eddf8
Ramone Robertson <ramonerobertson10@gmail.com>
2024-09-26 19:27:53 +0900

    Create movies from genre id in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,18 @@ tmdb_token = ENV["TMDB_KEY"]
 
 genres_tmdb_endpoint = "https://api.themoviedb.org/3/genre/movie/list?api_key=#{tmdb_token}"
 upcoming_tmdb_endpoint = "https://api.themoviedb.org/3/movie/now_playing"
+discover_tmdb_endpoint = "https://api.themoviedb.org/3/discover/movie"
 
 def api_call(endpoint)
   api_data = URI.open(endpoint).read
   parsed_json_data = JSON.parse(api_data)
+end
+
+def create_movie(movies_list)
+  movies_list.each do |movie|
+    movie = Movie.new(title: movie["title"], overview: movie["overview"], rating: movie["vote_average"], runtime: 102,  )
+    movie.save! if Movie.exists?(title: movie["title"]) == false
+  end
 end
 
 # ===============================================
@@ -39,3 +47,9 @@ end
 # ===============================================
 # MOVIES CREATION
 # ===============================================
+
+# Greate Movies for each Genre
+genres_data["genres"].each do |genre|
+  movies_data = api_call(discover_tmdb_endpoint + "?api_key=#{tmdb_token}&include_adult=false&with_genres=#{genre["id"]}")
+  create_movie(movies_data["results"])
+end
